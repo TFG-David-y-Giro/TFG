39ebfd60fe421e780586a462355a5f0764eb3f19
dapalo <davidskull94@gmail.com>
2022-04-06 10:19:45 +0200

    first commit

---
+++ TFG/Model/src/main/java/org/iesfm/model/pojos/Item.java
@@ -0,0 +1,95 @@
+package org.iesfm.model.pojos;
+
+import java.util.Objects;
+
+public class Item {
+
+    private int id;
+    private String name;
+    private int price;
+    private Category category;
+    private Supplier supplier;
+    private int quantity;
+
+    public Item(int id, String name, int price, Category category, Supplier supplier, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.supplier = supplier;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && price == item.price && quantity == item.quantity && Objects.equals(name, item.name) && Objects.equals(category, item.category) && Objects.equals(supplier, item.supplier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, category, supplier, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                ", supplier=" + supplier +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ TFG/Model/src/main/java/org/iesfm/model/repositories/ItemRepository.java
@@ -0,0 +1,4 @@
+package org.iesfm.model.repositories;
+
+public interface ItemRepository {
+}
