6f51aa520e70d75b15c1d84070a795978996999c
MigGiGo <miguelon6301@gmail.com>
2022-05-03 11:13:23 +0200

    Creado TopNavBar

--- WEB/productos.php
+++ WEB/productos.php
@@ -3,19 +3,46 @@
 
 <head>
     <title>Productos</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .topnav {
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        .topnav a {
+            float: left;
+            color: #f2f2f2;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .topnav a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
-
     <div class="topnav">
         <a href="index.php">Inicio</a>
         <a class="active" href="productos.php">Productos</a>
         <a href="carrito.php">Carrito</a>
     </div>
-
     <h1>This is a Heading</h1>
     <p>This is a paragraph.</p>
-
 </body>
 
 </html>
