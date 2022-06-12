7905d5de6a4cff578b67bd5659d428a97614f01f
MigGiGo <miguelon6301@gmail.com>
2022-06-12 17:45:12 +0200

    Optimizado

---
+++ TFG/JAVA/Back/src/main/java/org/iesfm/back/controllers/LoginController.java
@@ -0,0 +1,17 @@
+package org.iesfm.back.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    public LoginController() {
+    }
+
+
+    @GetMapping("/login")
+    public String login() {
+        return ;
+    }
+}
